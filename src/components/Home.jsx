52750d9aa7e674a0c4f9687b4cc9e78c3874b1c1
shivaprasad sapare <122955366+shivaprasad369@users.noreply.github.com>
2024-02-12 01:19:55 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
         <div className="flex flex-col bg-[#0f020240] md:gap-2 items-center justify-center  px-[3rem] object-contain md:mt-[5rem]">
           <ol
             type="1"
-            className=" flex flex-col  text-white max-[462px]:text-[0.8rem] mt-[2rem] py-10 max-[702px]:text-[10px] gap-10 font-semibold object-cover  max-[792px]:text-sm "
+            className=" flex flex-col  text-white max-[462px]:text-[0.8rem] max-md:mt-[2rem] py-10 max-[702px]:text-[10px] gap-10 font-semibold object-cover  max-[792px]:text-sm "
           >
             <li onClick={() => handleSetImage(0)} className="">
               <a href="#" className={"active:text-orange-300"}>
