f8f56123039c66428bd07a2cd27a1fc49e29a413
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-03-08 16:47:21 +0530

    Add

--- src/components/Client/How.jsx
+++ src/components/Client/How.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 export default function How() {
   return (
-    <div id="why" className=" w-full h-full relative overflow-hidden flex flex-col flex-wrap items-center justify-center mt-[1rem] bg-gray-50">
+    <div  className=" w-full h-full relative overflow-hidden flex flex-col flex-wrap items-center justify-center mt-[1rem] bg-gray-50">
       <div className="text-3xl font-bold mb-8">
         {/* <div style={{backgroundImage: `repeating-linear-gradient(red, pink 10%, green 20%)`,position:'absolute',zIndex}}/> */}
         <h1 style={{ fontFamily:` "Libre Baskerville", serif`}} className="text-4xl py-[2rem] tracking-widest text-[#473c16]" >How we work</h1>
@@ -26,9 +26,9 @@ export default function How() {
             </h3>
             <p className="text-[#7a4d189f] tracking-[0.2rem] md:text-xl">
               {" "}
-              <span className="absolute md:left-[2rem] md:bottom-[3.5rem] max-md:top-[-2.5rem]"><i className=" h-[10rem]  width-29"><h1 className="text-[10rem] text-[#c4b48791] font-extrabold">1.</h1></i></span> 
+              <span className="absolute md:left-[2rem] md:bottom-[42%] max-md:top-[-2.5rem]"><i className=" h-[10rem]  width-29"><h1 className="text-[10rem] text-[#c4b48791] font-extrabold">1.</h1></i></span> 
               We sit down to plan and create, pouring our hearts into tailoring
-              an event that reflects your dreams and desires
+              an event that reflects your dreams and desires. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi incidunt eligendi quam. Odit consectetur reprehenderit velit nesciunt distinctio veniam,
             </p>
           </div>
           <div className="p-6 md:w-1/2 w-full">
@@ -52,11 +52,11 @@ export default function How() {
             </h3>
             <p className="text-[#7a4d189f] tracking-[0.2rem] md:text-xl">
               {" "}
-              <span className="absolute md:left-[3rem] md:bottom-[3.5rem] max-md:top-0 w-9  "><i className=" h-[10rem]  width-29"><h1 className="text-[10rem]  text-[#cabe9b91] font-extrabold">2.</h1></i></span> 
+              <span className="absolute md:left-[3rem] md:bottom-[40%] max-md:top-0 w-9  "><i className=" h-[10rem]  width-29"><h1 className="text-[10rem]  text-[#cabe9b91] font-extrabold">2.</h1></i></span> 
               
 
               Behind the scenes, we prepare with care and dedication, putting in
-              the effort to make every detail perfect for your special day
+              the effort to make every detail perfect for your special day. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi incidunt eligendi quam. Odit consectetur reprehenderit velit nesciunt distinctio veniam,
             </p>
           </div>
           <div className="p-6 md:w-1/2 w-full md:hidden">
@@ -77,10 +77,10 @@ export default function How() {
             </h3>
             <p className="text-[#7a4d189f] tracking-[0.2rem] md:text-xl">
               {" "}
-              <span className="absolute  w-9 md:left-[3rem] md:bottom-[3.5rem] max-md:top-0 md:bottom-[5rem]"><i className=" h-[10rem]  width-29"><h1 className="text-[10rem] text-[#dac89891] font-extrabold">3.</h1></i></span> 
+              <span className="absolute  w-9 md:left-[3rem]  max-md:top-0 top-[40%]"><i className=" h-[10rem]  width-29"><h1 className="text-[10rem] text-[#dac89891] font-extrabold">3.</h1></i></span> 
 
               On the big day, with excitement and emotion, we bring your dreams
-              to life, ensuring every moment is as magical as you imagined
+              to life, ensuring every moment is as magical as you imagined. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi incidunt eligendi quam. Odit consectetur reprehenderit velit nesciunt distinctio veniam,
             </p>
           </div>
           <div className="p-6 md:w-1/2 w-[100%]">
