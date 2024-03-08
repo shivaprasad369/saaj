643f32f39942c8ae243dd9a42bcda65ae452548e
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-03-08 17:08:13 +0530

    Add

--- src/demo/Demo.jsx
+++ src/demo/Demo.jsx
@@ -15,14 +15,14 @@ export default function Demo() {
         <img src={img1} alt="" className='w-[22rem]  m-[6rem] relative  ' />
       </div> */}
 
-      <div  className="md:flex  relative z-10 max-[756px]:flex-col  max-[906px]:justify-around gap-10 md:justify-center min-[906px]:gap-[8rem] max-[756px]:min-[638px]:ml-[20%]  max-[380px]:ml-[0%] pt-[3rem]">
+      <div  className="md:flex  relative z-10 max-[756px]:flex-col  max-[906px]:justify-around  md:justify-around min-[906px]:gap-[0.5rem] max-[756px]:min-[638px]:ml-[20%]  max-[380px]:ml-[0%] pt-[3rem]">
 
         <img src={png} alt="" className="absolute top-[-8rem] left-0 w-[12rem] opacity-[0.5] rotate-180"/>
         <img src={png} alt="" className="absolute bottom-[-2.5rem] right-[-2rem] w-[12rem] opacity-[0.5]"/>
             {/* <img src={img2} alt=""  className='absolute w-[14rem] h-[17rem] right-[21rem] top-[5rem] z-10  rounded-t-full'/> */}
         <div  className="gap-5 max-[756px]:flex  max-[756px]:flex-col max-[756px]:text-center max-[756px]:justify-center  ">
           <motion.div initial={{opacity:0}} whileInView={{opacity:1,transition:{type:'spring',delay:0.3,ease:'easeInOut', duration:1}}} className="border-0 pb-[3rem] max-sm:w-[100%]  flex flex-col w-[30rem]  text-center gap-3">
-            <img src={img1} alt="" className="md:w-[30rem]  max-sm:h-[25rem] sm:h-[25rem]" />
+            <img src={img1} alt="" className="md:w-[100%]  max-sm:h-[25rem] sm:h-[20rem]" />
             <h1 className="tracking-widest text-[#443417] text-xl"> Lokesh & Pranjal - Jodhpur</h1>
             <p className="px-3 tracking-wider">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
